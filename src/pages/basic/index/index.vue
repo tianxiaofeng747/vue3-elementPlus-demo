11d5a6fad5f1b5ce1a7735badc7cbd69cbaa9ba4
田笑召 <tianxiaofeng747@163.com>
2021-05-28 17:01:02 +0800

    upd:  nav

--- src/pages/basic/index/index.vue
+++ src/pages/basic/index/index.vue
@@ -2,17 +2,32 @@
  * @Author: jinqing
  * @Date: 2021-05-11 15:16:39
  * @LastEditors: jinqing
- * @LastEditTime: 2021-05-24 16:33:50
+ * @LastEditTime: 2021-05-25 19:16:48
  * @Description: file content
 -->
 <template>
-    <div>
-        首页内容
-    </div>
+    <section class="container_setion">
+        <div class="my-errors">
+            <div class="error-icon"></div>
+            <p>页面不存在</p>
+        </div>
+    </section>
 </template>
 <script>
-import {defineComponent}from 'vue';
-export default defineComponent({
-    
-});
+export default {
+    data () {
+        return {
+            search: {},
+            list: []
+        };
+    },
+    computed: {},
+    methods: {
+    }
+};
+
 </script>
+
+<style lang="scss" scoped>
+   
+</style>
--- src/pages/basic/layout/Dashboard.vue
+++ src/pages/basic/layout/Dashboard.vue
@@ -2,7 +2,7 @@
  * @Author: jinqing
  * @Date: 2021-05-11 15:16:39
  * @LastEditors: jinqing
- * @LastEditTime: 2021-05-25 15:21:02
+ * @LastEditTime: 2021-05-25 17:30:44
  * @Description: file content
 -->
 <template>
@@ -17,9 +17,8 @@ import Error from '@/components/error/index.vue';
 import Nav from '@/components/navPage/index.vue';
 import { defineComponent, resolveDynamicComponent, watch,ref} from 'vue';
 import {useRoute} from 'vue-router';
-import { mapGetters, useStore } from 'vuex';
+import { useStore } from 'vuex';
 import Tools from '@/utils/customer/global.js';
-//const modules = import.meta.glob('../../**/index.vue');
 
 export default defineComponent({
     inject: ['$app'],
@@ -28,28 +27,22 @@ export default defineComponent({
             permissions: null
         };
     },
-    // computed: {
-    //     cachedViews () {
-    //         console.log(this.$store.state.app.cachedViews)
-    //         return this.$store.state.app.cachedViews;
-    //     }
-    // },
     methods: {
         // 初始化按钮权限
-        async initPermission () {
-            let no = this.$route.meta.no || this.$route.query.$no,
-                permissions = {},
-                path = this.$route.meta.componentUrl;
-            this._getBtnAuth(no, permissions);
-            this.$store.commit('setAuth', permissions);
-            console.log(permissions, '按钮权限');
-            // 模块点击，直接用navPage组件
-            if (this.$route.meta.leval === 2) {
-                this.componentName = Nav;
-                return;
-            }
-            path = /^\//.test(path) ? path : ('/' + path);
-        }
+        // async initPermission () {
+        //     let no = this.$route.meta.no || this.$route.query.$no,
+        //         permissions = {},
+        //         path = this.$route.meta.componentUrl;
+        //     this._getBtnAuth(no, permissions);
+        //     this.$store.commit('setAuth', permissions);
+        //     console.log(permissions, '按钮权限');
+        //     // 模块点击，直接用navPage组件
+        //     if (this.$route.meta.leval === 2) {
+        //         this.componentName = Nav;
+        //         return;
+        //     }
+        //     path = /^\//.test(path) ? path : ('/' + path);
+        // }
     },
     setup(){
         let route = useRoute(),
@@ -73,15 +66,13 @@ export default defineComponent({
         const updateComponent = () => {
             let comPath = Tools.reverseComponentName(route.meta.componentUrl);
             getCacheView();
+            console.log(Error);
             if(route.meta.leval === 2){
-                componentName.value = Nav;
+                componentName.value = Error;
             }
             else if(comPath){
                 componentName.value = comPath // resolveDynamicComponent();
-            } else {
-                componentName.value = Error;
             }
-            
         };
         watch(() => route.path, updateComponent);
         updateComponent();
@@ -91,7 +82,8 @@ export default defineComponent({
         }
     },
     components: {
-        Error
+        Error,
+        Nav
     }
 })
 </script>
